36a8450980966c6348a2e55f366396be5aafe4f0
JaaiDead <arcticquests.dev@gmail.com>
2025-04-12 20:48:09 +0530

    Nuked

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/dev-overflow.github.io/',  // Correct base URL for your repo
-  plugins: [react()],
-})
